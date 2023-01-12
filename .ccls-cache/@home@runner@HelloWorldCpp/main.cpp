32c852c0b0c5d4b37dd594f1b34b89e39bf5de1a
Prof John David Zehnpfennig II, PE <111508888+prof-jz@users.noreply.github.com>
2023-01-12 15:41:18 +0000

    Initial state for Lesson 2. Invited all class members to the repl and finished the header comments.

--- .ccls-cache/@home@runner@HelloWorldCpp/main.cpp
+++ .ccls-cache/@home@runner@HelloWorldCpp/main.cpp
@@ -1,12 +1,20 @@
-//HelloWorldCppSp2023 is the classic 'first program' for new programmers.
-//
+/*
+Alabama A&M University - EE109 Engineering Computing - Spring 2023
+PM: Prof. John Zehnpfennig
 
-//Alabama A&M University - EE109 Engineering Computing - Spring 2023
-//PM: Prof. John Zehnpfennig
-//
+Description: HelloWorldCpp is the classic 'first program' for new programmers.
+We will use this code in class in Lesson 2 to:
+  1. demonstrate and learn basic C++ I/O functions
+  2. demonstrate and use Github version control capabilities
+  3. demonstrate how to 'invite' multiplayer partners into a Repl
+  4. demonstrate collaborative coding
+  5. demonstrate proper commenting
+
+*/
 
 #include <iostream>
 
-int main() {
+int main() 
+{
   std::cout << "Hello World!\n";
 }
